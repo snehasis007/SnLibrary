bc516034b3fbee9e7c39b38abb50fa86f4e0d534
snehasis chandra <snehasis007@gmail.com>
2024-06-15 10:38:58 -0400

    test

---
+++ App1/App1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  App1
+//
+//  Created by snehasis chandra on 6/15/24.
+//
+
+#include <iostream>
+#pragma TEST
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
